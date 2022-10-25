ff5b6aba585b3ca9c0c4652ff9d66dd7591f3649
brandon <brandon.t.navarrete@gmail.com>
2022-10-25 14:27:20 -0500

    expanded answers.

--- limit_execercises.sql
+++ limit_execercises.sql
@@ -19,6 +19,20 @@ Select distinct last_name from employees
 	order by last_name desc limit 10;
 	
 #3.Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
+/*Alselm	Cappello	1962-12-25	1990-01-01
+Utz	Mandell	1960-12-25	1990-01-03
+Bouchung	Schreiter	1963-12-25	1990-01-04
+Baocai	Kushner	1959-12-25	1990-01-05
+Petter	Stroustrup	1959-12-25	1990-01-10*/
+
+SELECT first_name,last_name,birth_date,hire_date
+	FROM employees
+		WHERE hire_date LIKE '199%'
+			and birth_date LIKE '%12-25'
+				ORDER BY hire_date asc limit 5;
+
+
+
 /*
 Pranay	Narwekar	1963-12-25	1990-07-18
 Marjo	Farrow	1963-12-25	1990-07-18
